a2e397fc1a3c92a9db456fb80b6181e4ffe7c055
jd4518 <formless_@naver.com>
2015-11-04 16:39:27 +0900

    first commit

---
+++ src/com/hybrid/hybrid/HybridWebActivity.java
@@ -0,0 +1,70 @@
+package com.hybrid.hybrid;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class HybridWebActivity extends Activity {
+
+	WebView myweb;
+	
+	@SuppressLint("JavascriptInterface")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hybrid_web);
+		
+		myweb = (WebView) findViewById(R.id.myweb);
+		
+		WebSettings settings = myweb.getSettings();
+		settings.setJavaScriptEnabled(true);
+		settings.setBuiltInZoomControls(true);
+		
+		myweb.addJavascriptInterface(new MyJavascriptInterface(), "android");
+		myweb.setWebViewClient(new MyWebViewClient());
+		myweb.setWebChromeClient(new WebChromeClient());
+		myweb.loadUrl("http://192.198.10.15:8080/Web/index.jsp");
+	}
+	
+	class MyJavascriptInterface {
+		public void showToast(String msg){
+			Log.i("###", msg);
+			Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	class MyWebViewClient extends WebViewClient{
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.hybrid_web, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
